6ed03da29b1dca461ede45c57792ceaa03aa7d2d
Jay <jaytpa01@gmail.com>
2023-03-13 02:40:13 +1100

    Begun blocking out service layer tests

---
+++ internal/service/url_service_test.go
@@ -0,0 +1,9 @@
+package service
+
+// TODO: Write tests for ShortenUrl
+
+// TODO: Write tests for LengthenUrl
+
+// TODO: Write tests for GetUrlByToken
+
+// TODO: Write tests for IncrementUrlVisits
