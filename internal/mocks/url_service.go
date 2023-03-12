176fc8036fe8e8421cf68c3117bdef73197e70dd
Jay <jaytpa01@gmail.com>
2023-03-13 00:36:33 +1100

    Mocked the url service layer for testing purposes

---
+++ internal/mocks/url_service.go
@@ -0,0 +1,63 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/Jaytpa01/url-shortener-api/internal/entity"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockUrlService is a mock implementation of our service.UrlService
+type mockUrlService struct {
+	mock.Mock
+}
+
+// NewMockUrlService returns a mock implementation of our UrlService
+// for testing purposes.
+// It is built using testify.Mock
+func NewMockUrlService() *mockUrlService {
+	return new(mockUrlService)
+}
+
+// ShortenUrl is a mock implementation of UrlService.ShortenUrl
+func (m *mockUrlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, error) {
+	ret := m.Called(ctx, url)
+
+	var r0 *entity.Url
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*entity.Url)
+	}
+
+	return r0, ret.Error(1)
+}
+
+// LengthenUrl is a mock implementation of UrlService.LengthenUrl
+func (m *mockUrlService) LengthenUrl(ctx context.Context, url string) (*entity.Url, error) {
+	ret := m.Called(ctx, url)
+
+	var r0 *entity.Url
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*entity.Url)
+	}
+
+	return r0, ret.Error(1)
+}
+
+// GetUrlByToken is a mock implementation of UrlService.GetUrlByToken
+func (m *mockUrlService) GetUrlByToken(ctx context.Context, token string) (*entity.Url, error) {
+	ret := m.Called(ctx, token)
+
+	var r0 *entity.Url
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*entity.Url)
+	}
+
+	return r0, ret.Error(1)
+}
+
+// IncrementUrlVisits is a mock implementation of UrlService.IncrementUrlVisits
+func (m *mockUrlService) IncrementUrlVisits(ctx context.Context, url *entity.Url) error {
+	ret := m.Called(ctx, url)
+
+	return ret.Error(0)
+}
