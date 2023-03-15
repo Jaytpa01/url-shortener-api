3702fc0407d2ef4de5e04de609faf249f0e5cace
Jay <jaytpa01@gmail.com>
2023-03-16 03:17:03 +1100

    Updated config and reimplemented shortening logic

--- internal/service/url.go
+++ internal/service/url.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"time"
 
 	"github.com/Jaytpa01/url-shortener-api/api"
 	"github.com/Jaytpa01/url-shortener-api/internal/entity"
@@ -22,40 +21,63 @@ const (
 	MINIMUM_LONG_TOKEN_LENGTH   = 42 // the mimimum length that a lengthened token should be
 )
 
-type urlService struct {
-	logger  logger.Logger
-	urlRepo repository.UrlRepository
-}
-
 type Config struct {
 	Logger  logger.Logger
 	UrlRepo repository.UrlRepository
+	Random  utils.Random
+}
+
+// urlService is used for the actual service implementation of this api
+type urlService struct {
+	logger  logger.Logger
+	urlRepo repository.UrlRepository
+	random  utils.Random
 }
 
 func NewUrlService(c *Config) UrlService {
+	if c.Random == nil {
+		c.Random = utils.NewRandomiser()
+	}
+
 	return &urlService{
 		urlRepo: c.UrlRepo,
 		logger:  c.Logger,
+		random:  c.Random,
 	}
 }
 
 // ShortenUrl validates the url, then attempts to create it in the repository.
 func (u *urlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, error) {
 	if !validation.IsValidUrl(url) {
-		return nil, api.NewBadRequest("invalid-url", "The provided URL (%s) is invalid.")
+		return nil, api.NewBadRequest("url/invalid", fmt.Sprintf("The provided URL (%s) is invalid.", url))
 	}
 
 	newUrl := &entity.Url{
-		Token:     utils.GenerateRandomString(TOKEN_LENGTH),
+		Token:     u.random.GenerateRandomString(TOKEN_LENGTH),
 		TargetUrl: url,
-		CreatedAt: time.Now(),
 	}
 
-	err := createShortUrl(ctx, newUrl, u.urlRepo)
-	if err != nil {
-		// Because createShortUrl recursively retries until theres no token clash, we simply return
-		// an internal error here
+	var err error
+	// because there's a very slim chance a generated token may clash, we give it up to 3 attempts
+	attempts := 3
+	for i := 0; i < attempts; i++ {
+		err = u.urlRepo.Create(ctx, newUrl)
+		if err != nil {
+			if errors.Is(err, repository.ErrTokenAlreadyExists) {
+				newUrl.Token = u.random.GenerateRandomString(TOKEN_LENGTH)
+				continue
+			} else {
+				return nil, api.NewInternal("couldnt-shorten", api.WithDebug(err.Error()))
+			}
+		}
 
+		// no error, so we break
+		break
+	}
+
+	if err != nil {
+		// Because we try 3 times to prevent a token clash, we simply return an internal error here after several
+		// attempts if we keep clashinig
 		apiErr := api.NewInternal("couldnt-shorten", api.WithDebug(err.Error()))
 		u.logger.Info("Failed to shorten URL.", apiErr)
 		return nil, apiErr
@@ -67,16 +89,31 @@ func (u *urlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, e
 // LengthenUrl validates the url, then attempts to create it in the repository.
 func (u *urlService) LengthenUrl(ctx context.Context, url string) (*entity.Url, error) {
 	if !validation.IsValidUrl(url) {
-		return nil, api.NewBadRequest("invalid-url", "The provided URL (%s) is invalid.")
+		return nil, api.NewBadRequest("url/invalid", "The provided URL (%s) is invalid.")
 	}
 
 	newUrl := &entity.Url{
-		Token:     utils.GenerateRandomString(utils.Max(MINIMUM_LONG_TOKEN_LENGTH, len(url)*LENGTHEN_TOKEN_SCALE_FACTOR)),
+		Token:     u.random.GenerateRandomString(utils.Max(MINIMUM_LONG_TOKEN_LENGTH, len(url)*LENGTHEN_TOKEN_SCALE_FACTOR)),
 		TargetUrl: url,
-		CreatedAt: time.Now(),
 	}
 
-	err := createLongUrl(ctx, newUrl, u.urlRepo)
+	var err error
+	// because there's a very slim chance a generated token may clash, we give it up to 3 attempts
+	for i := 0; i < 3; i++ {
+		err = u.urlRepo.Create(ctx, newUrl)
+		if err != nil {
+			if errors.Is(err, repository.ErrTokenAlreadyExists) {
+				newUrl.Token = u.random.GenerateRandomString(utils.Max(MINIMUM_LONG_TOKEN_LENGTH, len(url)*LENGTHEN_TOKEN_SCALE_FACTOR))
+				continue
+			} else {
+				return nil, err
+			}
+		}
+
+		// no error, so we break
+		break
+	}
+
 	if err != nil {
 		// Because createLongUrl recursively retries until theres no token clash, we simply return
 		// an internal error here
@@ -89,15 +126,15 @@ func (u *urlService) LengthenUrl(ctx context.Context, url string) (*entity.Url, 
 	return newUrl, nil
 }
 
-// GetUrlByToken attempts to find the URL associated to provided token.
-func (u *urlService) GetUrlByToken(ctx context.Context, token string) (*entity.Url, error) {
+// FindUrlByToken attempts to find the URL associated to provided token.
+func (u *urlService) FindUrlByToken(ctx context.Context, token string) (*entity.Url, error) {
 	url, err := u.urlRepo.FindByToken(ctx, token)
 	if err != nil {
-		if errors.Is(err, api.ErrUrlNotFound) {
-			return nil, api.NewNotFound("url-not-found", fmt.Sprintf("Couldn't find URL with token (%s).", token))
+		if errors.Is(err, repository.ErrUrlNotFound) {
+			return nil, api.NewNotFound("url/not-found", fmt.Sprintf("Couldn't find URL with token (%s).", token))
 		}
 
-		apiErr := api.NewInternal("internal-error-url-not-found", api.WithDebug(err.Error()))
+		apiErr := api.NewInternal("url/internal", api.WithDebug(err.Error()))
 		u.logger.Info("Couldn't retrieve URL", apiErr)
 
 		return nil, apiErr
@@ -111,24 +148,6 @@ func (u *urlService) IncrementUrlVisits(ctx context.Context, url *entity.Url) er
 	return u.urlRepo.Update(ctx, url)
 }
 
-// createShortUrl attempts to create a new url resource. If the token already exists,
-// it will recursively try with a new token. If its some other error, it will simply return it.
-func createShortUrl(ctx context.Context, url *entity.Url, repo repository.UrlRepository) error {
-	err := repo.Create(ctx, url)
-	if errors.Is(err, api.ErrTokenAlreadyExists) {
-		url.Token = utils.GenerateRandomString(TOKEN_LENGTH)
-		return createShortUrl(ctx, url, repo)
-	}
-	return err
-}
-
-// createLongUrl attempts to create a new url resource. If the token already exists,
-// it will recursively try with a new token. If its some other error, it will simply return it.
-func createLongUrl(ctx context.Context, url *entity.Url, repo repository.UrlRepository) error {
-	err := repo.Create(ctx, url)
-	if errors.Is(err, api.ErrTokenAlreadyExists) {
-		url.Token = utils.GenerateRandomString(utils.Max(MINIMUM_LONG_TOKEN_LENGTH, len(url.TargetUrl)*LENGTHEN_TOKEN_SCALE_FACTOR))
-		return createShortUrl(ctx, url, repo)
-	}
-	return err
+func (u *urlService) GetAllUrls(ctx context.Context) ([]entity.Url, error) {
+	return u.urlRepo.GetAllUrls(ctx)
 }
