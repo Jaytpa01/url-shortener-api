a5ad23bc4aaa8292f18360fee55ef525faa9dc03
Jay <jaytpa01@gmail.com>
2023-03-16 03:15:09 +1100

    Added config support using viper

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server ServerConfig `mapstructure:"server"`
+}
+
+type ServerConfig struct {
+	Environment string
+	Port        int
+	BaseUrl     string `mapstructure:"base_url"`
+}
+
+// LoadConfig takes in a filename and attempts to load in a config file using viper from the current directy, "./etc/config", and "/etc/config"
+func LoadConfig(filename string) (*Config, error) {
+	viper.SetConfigFile(filename)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	return parseConfig()
+}
+
+// parseConfig
+func parseConfig() (*Config, error) {
+	config := &Config{}
+	err := viper.Unmarshal(config)
+	if err != nil {
+		return nil, errors.New("unable to decode config file into Config struct")
+	}
+
+	return config, nil
+}
