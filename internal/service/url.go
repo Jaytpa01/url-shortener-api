46e97da22a9fc9d2dee814db658c59a4e76b4214
Jay <jaytpa01@gmail.com>
2023-03-16 18:37:26 +1100

    Finished url service tests

--- internal/service/url.go
+++ internal/service/url.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/Jaytpa01/url-shortener-api/api"
 	"github.com/Jaytpa01/url-shortener-api/internal/entity"
@@ -55,6 +56,7 @@ func (u *urlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, e
 	newUrl := &entity.Url{
 		Token:     u.random.GenerateRandomString(TOKEN_LENGTH),
 		TargetUrl: url,
+		CreatedAt: time.Now(),
 	}
 
 	var err error
@@ -67,7 +69,7 @@ func (u *urlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, e
 				newUrl.Token = u.random.GenerateRandomString(TOKEN_LENGTH)
 				continue
 			} else {
-				return nil, api.NewInternal("couldnt-shorten", api.WithDebug(err.Error()))
+				return nil, api.NewInternal("url/couldnt-shorten", api.WithDebug(err.Error()))
 			}
 		}
 
@@ -76,9 +78,9 @@ func (u *urlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, e
 	}
 
 	if err != nil {
-		// Because we try 3 times to prevent a token clash, we simply return an internal error here after several
-		// attempts if we keep clashinig
-		apiErr := api.NewInternal("couldnt-shorten", api.WithDebug(err.Error()))
+		// Because we try 3 times to prevent a token clash, we simply return an internal
+		// error here after several attempts if we keep clashing
+		apiErr := api.NewInternal("url/couldnt-shorten", api.WithDebug(err.Error()))
 		u.logger.Info("Failed to shorten URL.", apiErr)
 		return nil, apiErr
 	}
@@ -89,7 +91,7 @@ func (u *urlService) ShortenUrl(ctx context.Context, url string) (*entity.Url, e
 // LengthenUrl validates the url, then attempts to create it in the repository.
 func (u *urlService) LengthenUrl(ctx context.Context, url string) (*entity.Url, error) {
 	if !validation.IsValidUrl(url) {
-		return nil, api.NewBadRequest("url/invalid", "The provided URL (%s) is invalid.")
+		return nil, api.NewBadRequest("url/invalid", fmt.Sprintf("The provided URL (%s) is invalid.", url))
 	}
 
 	newUrl := &entity.Url{
@@ -99,14 +101,15 @@ func (u *urlService) LengthenUrl(ctx context.Context, url string) (*entity.Url, 
 
 	var err error
 	// because there's a very slim chance a generated token may clash, we give it up to 3 attempts
-	for i := 0; i < 3; i++ {
+	attempts := 3
+	for i := 0; i < attempts; i++ {
 		err = u.urlRepo.Create(ctx, newUrl)
 		if err != nil {
 			if errors.Is(err, repository.ErrTokenAlreadyExists) {
 				newUrl.Token = u.random.GenerateRandomString(utils.Max(MINIMUM_LONG_TOKEN_LENGTH, len(url)*LENGTHEN_TOKEN_SCALE_FACTOR))
 				continue
 			} else {
-				return nil, err
+				return nil, api.NewInternal("url/couldnt-lengthen", api.WithDebug(err.Error()))
 			}
 		}
 
@@ -115,10 +118,9 @@ func (u *urlService) LengthenUrl(ctx context.Context, url string) (*entity.Url, 
 	}
 
 	if err != nil {
-		// Because createLongUrl recursively retries until theres no token clash, we simply return
-		// an internal error here
-
-		apiErr := api.NewInternal("couldnt-lengthen", api.WithDebug(err.Error()))
+		// Because we try 3 times to prevent a token clash, we simply return an internal
+		// error here after several attempts if we keep clashing
+		apiErr := api.NewInternal("url/couldnt-lengthen", api.WithDebug(err.Error()))
 		u.logger.Info("Failed to lengthen URL.", apiErr)
 		return nil, apiErr
 	}
@@ -143,9 +145,23 @@ func (u *urlService) FindUrlByToken(ctx context.Context, token string) (*entity.
 	return url, nil
 }
 
+var ErrNilUrlPointer = errors.New("received nil url pointer")
+
+// IncrementUrlVisits increments the visits of the url and persists that to our UrlRepository
 func (u *urlService) IncrementUrlVisits(ctx context.Context, url *entity.Url) error {
+	if url == nil {
+		return ErrNilUrlPointer
+	}
+
 	url.Visits++
-	return u.urlRepo.Update(ctx, url)
+	err := u.urlRepo.Update(ctx, url)
+	if err != nil {
+		// if we couldn't update, decrement visits as we directly modified the pointer before
+		url.Visits--
+		return err
+	}
+
+	return nil
 }
 
 func (u *urlService) GetAllUrls(ctx context.Context) ([]entity.Url, error) {
--- internal/service/url_service_test.go
+++ internal/service/url_service_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/Jaytpa01/url-shortener-api/internal/mocks"
 	"github.com/Jaytpa01/url-shortener-api/internal/repository"
 	"github.com/Jaytpa01/url-shortener-api/pkg/logger"
+	"github.com/Jaytpa01/url-shortener-api/pkg/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -51,12 +52,9 @@ func Test_ShortenUrl(t *testing.T) {
 		{
 			"Token Clash",
 			"https://example.com",
-			&entity.Url{
-				Token:     "123456",
-				TargetUrl: "https://example.com",
-			},
+			nil,
 			repository.ErrTokenAlreadyExists,
-			api.NewInternal("couldnt-shorten", api.WithDebug(repository.ErrTokenAlreadyExists.Error())),
+			api.NewInternal("url/couldnt-shorten", api.WithDebug(repository.ErrTokenAlreadyExists.Error())),
 		},
 		{
 			"Unknown Repo Error",
@@ -66,20 +64,21 @@ func Test_ShortenUrl(t *testing.T) {
 				TargetUrl: "https://example.com",
 			},
 			errors.New("some repo error"),
-			api.NewInternal("couldnt-shorten", api.WithDebug("some repo error")),
+			api.NewInternal("url/couldnt-shorten", api.WithDebug("some repo error")),
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+
 			repo := mocks.NewMockUrlRepository()
-			repo.On("Create", mock.AnythingOfType("*context.emptyCtx"), test.url).Return(test.repoError)
+			repo.On("Create", mock.AnythingOfType("*context.emptyCtx"), mock.AnythingOfType("*entity.Url")).Return(test.repoError)
 
 			randomiser := mocks.NewMockRandomiser()
 			randomiser.On("GenerateRandomString", 6).Return("123456")
 			urlService := NewUrlService(&Config{
 				UrlRepo: repo,
-				Logger:  logger.NewApiLogger(),
+				Logger:  logger.NewApiLogger("development"),
 				Random:  randomiser,
 			})
 
@@ -91,6 +90,13 @@ func Test_ShortenUrl(t *testing.T) {
 			}
 
 			if test.url != nil && test.repoError == nil {
+				// Because the service implementation uses time.Now() for the CreatedAt timestamp, the test.url and url returned
+				// will almost always be slightly different because of this. We could use an interface and use dependeny injection,
+				// but thats unneccessary and annoying, so we just coerce the values to be equal here
+				createdAt := time.Now()
+				test.url.CreatedAt = createdAt
+				url.CreatedAt = createdAt
+
 				assert.Equal(t, test.url, url)
 			}
 
@@ -98,7 +104,79 @@ func Test_ShortenUrl(t *testing.T) {
 	}
 }
 
-// TODO: Write tests for LengthenUrl
+func Test_LengthenUrl(t *testing.T) {
+	testCases := []struct {
+		name           string
+		inputUrl       string
+		serviceUrlResp *entity.Url
+		repoError      error
+		expectedError  error
+	}{
+		{
+
+			"Successfully Lengthen URL",
+			"https://example.com",
+			&entity.Url{
+				Token:     "ThisIsMeantToRepresentAReallyReallyReallyReallyLongToken",
+				TargetUrl: "https://example.com",
+			},
+			nil,
+			nil,
+		},
+		{
+			"Create Invalid Url",
+			"example",
+			nil,
+			repository.ErrTokenAlreadyExists,
+			api.NewBadRequest("url/invalid", fmt.Sprintf("The provided URL (%s) is invalid.", "example")),
+		},
+		{
+			"Some Unknown Repo Error",
+			"https://example.com",
+			nil,
+			errors.New("some unknown error"),
+			api.NewInternal("url/couldnt-lengthen", api.WithDebug(errors.New("some unknown error").Error())),
+		},
+		{
+			"Token Clash",
+			"https://example.com",
+			nil,
+			repository.ErrTokenAlreadyExists,
+			api.NewInternal("url/couldnt-lengthen", api.WithDebug(repository.ErrTokenAlreadyExists.Error())),
+		},
+	}
+
+	for _, test := range testCases {
+		repo := mocks.NewMockUrlRepository()
+		repo.On("Create", mock.AnythingOfType("*context.emptyCtx"), mock.AnythingOfType("*entity.Url")).Return(test.repoError)
+
+		randomiser := mocks.NewMockRandomiser()
+		randomiser.On("GenerateRandomString", utils.Max(MINIMUM_LONG_TOKEN_LENGTH, len(test.inputUrl)*LENGTHEN_TOKEN_SCALE_FACTOR)).Return("ThisIsMeantToRepresentAReallyReallyReallyReallyLongToken")
+		urlService := NewUrlService(&Config{
+			UrlRepo: repo,
+			Logger:  logger.NewApiLogger("development"),
+			Random:  randomiser,
+		})
+
+		url, err := urlService.LengthenUrl(context.Background(), test.inputUrl)
+
+		if test.expectedError != nil {
+			assert.Error(t, err)
+			assert.Equal(t, test.expectedError, err)
+		}
+
+		if test.serviceUrlResp != nil && test.repoError == nil {
+			// Because the service implementation uses time.Now() for the CreatedAt timestamp, the test.serviceUrlResp and url returned
+			// will almost always be slightly different because of this. We could use an interface and use dependeny injection,
+			// but thats unneccessary and annoying, so we just coerce the values to be equal here
+			createdAt := time.Now()
+			test.serviceUrlResp.CreatedAt = createdAt
+			url.CreatedAt = createdAt
+
+			assert.Equal(t, test.serviceUrlResp, url)
+		}
+	}
+}
 
 func Test_FindUrlByToken(t *testing.T) {
 	testCases := []struct {
@@ -142,7 +220,7 @@ func Test_FindUrlByToken(t *testing.T) {
 
 		urlService := NewUrlService(&Config{
 			UrlRepo: repo,
-			Logger:  logger.NewApiLogger(),
+			Logger:  logger.NewApiLogger("development"),
 		})
 
 		result, err := urlService.FindUrlByToken(context.Background(), test.token)
@@ -157,4 +235,120 @@ func Test_FindUrlByToken(t *testing.T) {
 
 }
 
-// TODO: Write tests for IncrementUrlVisits
+func Test_IncrementUrlVisits(t *testing.T) {
+	testCases := []struct {
+		name                            string
+		inputUrl                        *entity.Url
+		expectedVisitsAfterIncrementing int
+		repoError                       error
+		expectedServiceError            error
+	}{
+		{
+			"Successfully Incremented Visits",
+			&entity.Url{
+				Token:  "987654",
+				Visits: 0,
+			},
+			1,
+			nil,
+			nil,
+		},
+		{
+			"Couldn't Increment Visits",
+			&entity.Url{
+				Token:  "987654",
+				Visits: 0,
+			},
+			0,
+			errors.New("couldn't increment"),
+			errors.New("couldn't increment"),
+		},
+		{
+			"Provide Nil URL Pointer",
+			nil,
+			0,
+			ErrNilUrlPointer,
+			ErrNilUrlPointer,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			repo := mocks.NewMockUrlRepository()
+			repo.On("Update", mock.AnythingOfType("*context.emptyCtx"), test.inputUrl).Return(test.repoError)
+
+			service := NewUrlService(&Config{
+				UrlRepo: repo,
+				Logger:  logger.NewApiLogger("development"),
+			})
+
+			err := service.IncrementUrlVisits(context.Background(), test.inputUrl)
+			if test.expectedServiceError != nil {
+				assert.Error(t, err)
+				assert.Equal(t, test.expectedServiceError, err)
+			}
+
+			if test.inputUrl != nil {
+				assert.Equal(t, test.expectedVisitsAfterIncrementing, test.inputUrl.Visits)
+			}
+		})
+	}
+}
+
+func Test_GetAllUrls(t *testing.T) {
+	testCases := []struct {
+		name            string
+		repoUrlResponse []entity.Url
+		repoErr         error
+		expectedErr     error
+	}{
+		{
+			"Success",
+			[]entity.Url{
+				{
+					Token:     "123456",
+					TargetUrl: "https://example.com",
+				},
+				{
+					Token:     "987654",
+					TargetUrl: "https://google.com",
+				},
+			},
+			nil,
+			nil,
+		},
+		{
+			"Failure",
+			nil,
+			errors.New("some error"),
+			errors.New("some error"),
+		},
+		{
+			"Success But No URLS in Repo",
+			[]entity.Url{},
+			nil,
+			nil,
+		},
+	}
+
+	for _, test := range testCases {
+		repo := mocks.NewMockUrlRepository()
+		repo.On("GetAllUrls", mock.AnythingOfType("*context.emptyCtx")).Return(test.repoUrlResponse, test.repoErr)
+
+		service := NewUrlService(&Config{
+			UrlRepo: repo,
+			Logger:  logger.NewApiLogger("development"),
+		})
+
+		urls, err := service.GetAllUrls(context.Background())
+
+		if test.expectedErr != nil {
+			assert.Error(t, err)
+			assert.Equal(t, test.expectedErr, err)
+		}
+
+		assert.Equal(t, test.repoUrlResponse, urls)
+
+	}
+
+}
