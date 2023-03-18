08d2d91366f42cab3093a8d6b6a596d6142e9329
Jay <jaytpa01@gmail.com>
2023-03-19 00:10:08 +1100

    added db struct tags to the url entity

--- internal/entity/url.go
+++ internal/entity/url.go
@@ -2,12 +2,12 @@ package entity
 
 import "time"
 
-const UrlTableName = "urls"
+const UrlTableName = "url"
 
 // Url defines the domain model
 type Url struct {
-	Token     string
-	TargetUrl string
-	Visits    int
-	CreatedAt time.Time
+	Token     string    `db:"token"`
+	TargetUrl string    `db:"target_url"`
+	Visits    int       `db:"visits"`
+	CreatedAt time.Time `db:"created_at"`
 }
