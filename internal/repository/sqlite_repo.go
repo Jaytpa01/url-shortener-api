3b8ba671c63f078354cc54fd497931de5ffab358
Jay <jaytpa01@gmail.com>
2023-03-19 00:09:27 +1100

    Implemented Create() for sqlite repo

--- internal/repository/sqlite_repo.go
+++ internal/repository/sqlite_repo.go
@@ -14,8 +14,8 @@ type sqliteRepository struct {
 	db *sqlx.DB
 }
 
-func NewSQLiteRepository() (UrlRepository, error) {
-	db, err := sqlx.Connect("sqlite3", "./db/url.db")
+func NewSQLiteRepository(dsn string) (UrlRepository, error) {
+	db, err := sqlx.Connect("sqlite3", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't connect to sqlite database: %w", err)
 	}
@@ -30,7 +30,7 @@ func NewSQLiteRepository() (UrlRepository, error) {
 func (s *sqliteRepository) FindByToken(ctx context.Context, token string) (*entity.Url, error) {
 	url := &entity.Url{}
 
-	err := s.db.GetContext(ctx, &url, "SELECT * FROM url WHERE token = ?", token)
+	err := s.db.GetContext(ctx, url, "SELECT * FROM url WHERE token = ?", token)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrUrlNotFound
@@ -43,7 +43,33 @@ func (s *sqliteRepository) FindByToken(ctx context.Context, token string) (*enti
 }
 
 func (s *sqliteRepository) Create(ctx context.Context, url *entity.Url) error {
-	panic("unimplemented") // TODO: implement
+	// Create a helper function for preparing failure results.
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+	// Defer a rollback in case anything fails.
+	defer tx.Rollback()
+
+	result, err := tx.ExecContext(ctx, `INSERT INTO url (token, target_url, created_at) VALUES (?, ?, ?)`, url.Token, url.TargetUrl, url.CreatedAt)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected != 1 {
+		return fmt.Errorf("%d rows affected, expected 1", rowsAffected)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *sqliteRepository) Update(ctx context.Context, url *entity.Url) error {
