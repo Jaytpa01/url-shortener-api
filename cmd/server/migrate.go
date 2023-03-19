eabf82f20d2c50d73f912df0d7c8e0e138e2af7f
Jay <jaytpa01@gmail.com>
2023-03-20 01:45:20 +1100

    Refactored migrate command

--- cmd/server/migrate.go
+++ cmd/server/migrate.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
 )
 
@@ -15,24 +13,14 @@ var dsn string
 
 func migrateCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "migrate",
-		Short: "Runs SQL Migrations.",
-		Long:  "migrate runs SQL migrations found in the db/migrations directory to the provided DSN.",
+		Use:     "migrate",
+		Short:   "Runs SQL Migrations.",
+		Long:    "migrate runs SQL migrations found in the db/migrations directory to the provided DSN. For SQLite, the DSN should look like 'sqlite3://path/to/file.db'",
+		Example: "url-shortener-api migrate -d sqlite://db/url.db",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			db, err := sql.Open("sqlite3", dsn)
+			m, err := migrate.New("file://db/migrations", dsn)
 			if err != nil {
-				return fmt.Errorf("couldn't open db connection: %w", err)
-			}
-
-			driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
-			if err != nil {
-				return fmt.Errorf("couldn't create sqlite3 driver: %w", err)
-			}
-
-			m, err := migrate.NewWithDatabaseInstance("file://db/migrations", "sqlite3", driver)
-			if err != nil {
-				return fmt.Errorf("error creating migration instance: %w", err)
+				return err
 			}
 
 			err = m.Up()
