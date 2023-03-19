bf95cb11c5c13dc0bf5cb0b62d768304a36e6f1b
Jay <jaytpa01@gmail.com>
2023-03-20 01:46:23 +1100

    Implemented Update and GetAllUrls to satisfy UrlRepository interface

--- internal/repository/sqlite_repo.go
+++ internal/repository/sqlite_repo.go
@@ -10,10 +10,13 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// sqliteRepository is the struct used for an SQLite implementation of our UrlRepository
 type sqliteRepository struct {
 	db *sqlx.DB
 }
 
+// NewSQLiteRepository attempts to connect to an SQLite file supplied by "dsn".
+// If successful, creates and returns &sqliteRepositpry
 func NewSQLiteRepository(dsn string) (UrlRepository, error) {
 	db, err := sqlx.Connect("sqlite3", dsn)
 	if err != nil {
@@ -43,7 +46,6 @@ func (s *sqliteRepository) FindByToken(ctx context.Context, token string) (*enti
 }
 
 func (s *sqliteRepository) Create(ctx context.Context, url *entity.Url) error {
-	// Create a helper function for preparing failure results.
 	tx, err := s.db.Begin()
 	if err != nil {
 		return err
@@ -73,9 +75,40 @@ func (s *sqliteRepository) Create(ctx context.Context, url *entity.Url) error {
 }
 
 func (s *sqliteRepository) Update(ctx context.Context, url *entity.Url) error {
-	panic("unimplemented") // TODO: implement
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+	// Defer a rollback in case anything fails.
+	defer tx.Rollback()
+
+	result, err := tx.ExecContext(ctx, `UPDATE url SET target_url = ?, visits = ? WHERE token = ?`, url.TargetUrl, url.Visits, url.Token)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected != 1 {
+		return fmt.Errorf("%d rows affected, expected 1", rowsAffected)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *sqliteRepository) GetAllUrls(ctx context.Context) ([]entity.Url, error) {
-	panic("unimplemented") // TODO: implement
+	urls := []entity.Url{}
+
+	if err := s.db.SelectContext(ctx, &urls, "SELECT * FROM url"); err != nil {
+		return nil, err
+	}
+
+	return urls, nil
 }
